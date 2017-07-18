65d4064dbfce47a8481cc862ae206da6e8b39c60
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-07-18 17:27:41 -0400

    add delete picture function

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
+
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
   get 'pictures/:id' => 'pictures#show'
+
+  delete 'pictures/:id' => 'pictures#destroy'
 end
